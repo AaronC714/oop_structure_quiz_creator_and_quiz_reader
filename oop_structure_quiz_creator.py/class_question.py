481240c1247f2a65f199372682803d783b2bc62a
AaronC714 <aaronc0714@gmail.com>
2025-05-23 18:42:42 +0800

    model of the question proper

--- oop_structure_quiz_creator.py/class_question.py
+++ oop_structure_quiz_creator.py/class_question.py
@@ -3,3 +3,11 @@ class Question:
         self.question = question
         self.choices = choices
         self.correct = correct
+
+    def to_text(self):
+        text = f"question: {self.question} \n"
+        for key in ['a', 'b', 'c', 'd']:
+            text += f"{key}) {self.choices[key]}\n"
+        text += f"Correct Answer: {self.correct}\n"
+        text += "-" * 40 + "\n"
+        return text
