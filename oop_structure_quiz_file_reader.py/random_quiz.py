4a78e21b96e6de708dec8364ec24d3f9949a3d9e
AaronC714 <aaronc0714@gmail.com>
2025-05-23 21:58:56 +0800

    initial code for random quiz

--- oop_structure_quiz_file_reader.py/random_quiz.py
+++ oop_structure_quiz_file_reader.py/random_quiz.py
@@ -3,3 +3,9 @@ from time import sleep
 from colorama import Fore, Style
 from base_quiz import BaseQuiz
 from core_and_utilities import clear_screen
+
+class RandomQuiz(BaseQuiz):
+    def start(self):
+        if not self.questions:
+            print ("No questions found for the quiz.")
+            return
