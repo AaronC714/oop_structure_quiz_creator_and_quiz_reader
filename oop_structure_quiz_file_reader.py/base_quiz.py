c016c12e678b85a69d3ffd861b5ea8ab2fc258aa
AaronC714 <aaronc0714@gmail.com>
2025-05-23 21:41:10 +0800

    load questions

--- oop_structure_quiz_file_reader.py/base_quiz.py
+++ oop_structure_quiz_file_reader.py/base_quiz.py
@@ -6,3 +6,8 @@ class BaseQuiz(ABC):
     def __init__(self, filename):
         self.filename = filename
         self.questions = self.load_questions()
+
+        def laod_questions(self):
+            if not os.path.exists(self.filename):
+                print(Fore.RED +  "⚠️ No quiz file found.")
+                return[]
