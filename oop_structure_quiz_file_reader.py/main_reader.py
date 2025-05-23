6fe63899830c9000614aca7c53f8abcf3eeb4402
AaronC714 <aaronc0714@gmail.com>
2025-05-23 21:13:09 +0800

    def main for quiz start

--- oop_structure_quiz_file_reader.py/main_reader.py
+++ oop_structure_quiz_file_reader.py/main_reader.py
@@ -1,1 +1,7 @@
-#
+from core.utils import get_quiz_file_path
+from features.random_quiz import random_quiz
+
+def main():
+    file_path = get_quiz_file_path()
+    quiz = random_quiz(file_path)
+    quiz.start()
