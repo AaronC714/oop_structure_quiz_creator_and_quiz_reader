3a7380f1f54372d6ae5d0cc7b936797655fbdf8e
AaronC714 <aaronc0714@gmail.com>
2025-05-23 16:38:59 +0800

    class for quiz creator

--- oop_structure_quiz_creator.py/main_menu_creator.py
+++ oop_structure_quiz_creator.py/main_menu_creator.py
@@ -3,3 +3,8 @@
 import os
 from models.question import Question
 from io.question_saver import Question_saver
+
+class QuizCreator:
+    def __inti__(self):
+        self.file_path = os.path.join(os.path.expanduser("~"), "Desktop", "quiz.txt")
+        self.saver = QuestionSaver(self.file_path)
