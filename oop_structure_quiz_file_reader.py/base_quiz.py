844218778369fa98bbe79feb48b202ee7d43d85a
AaronC714 <aaronc0714@gmail.com>
2025-05-23 21:39:35 +0800

    init for self and filename added

--- oop_structure_quiz_file_reader.py/base_quiz.py
+++ oop_structure_quiz_file_reader.py/base_quiz.py
@@ -1,3 +1,8 @@
 import os
 from colorama import Fore
 from abc import ABC, abstractmethod
+
+class BaseQuiz(ABC):
+    def __init__(self, filename):
+        self.filename = filename
+        self.questions = self.load_questions()
