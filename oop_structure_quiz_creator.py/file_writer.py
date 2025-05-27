ad0bee7af727b241f05053356e0cbfea1e4c122c
Aaron <aaronc0714@gmail.com>
2025-05-27 23:01:06 +0800

    Delete oop_structure_quiz_creator.py directory

--- oop_structure_quiz_creator.py/file_writer.py
+++
@@ -1,7 +0,0 @@
-class FileWriter:
-    def __init__(self, filename):
-        self.filename = filename
-
-    def write(self, text):
-        with open(self.filename, "a") as file:
-            file.write(text)
--- oop_structure_quiz_creator.py/question_saver.py
+++
@@ -1,5 +0,0 @@
-from .file_writer import FileWriter
-
-class QuestionSaver(FileWriter):
-    def save_question(self, question):
-        self.write(question.to_text())
